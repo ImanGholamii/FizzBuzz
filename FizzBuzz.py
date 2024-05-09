4e298c11fa94f4c47db018ab58437c6ed49b16af
Iman <imangholamiimi@gmail.com>
2024-05-09 19:07:48 +0330

    :zap: | UPDATE | change print statements. add hint for current number place

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -3,10 +3,9 @@ current_number = 0
 while True:
     shomarande = [1, 2, 3, 4] 
     shomarande = shomarande[index]
-    print('shomarande: ', shomarande)
+    print(f'hint: current number is: {current_number + 1}')
     index += 1
     current_number += 1
-    # print('current_number: ', current_number)
     if index == 4:
         index = 0 
         
@@ -22,7 +21,6 @@ while True:
             elif p1_choice == "fizzbuzz":
                 p1_choice == -30
                             
-        # if (current_number % 3 == 0 and p1_choice == "Fizz") or (current_number % 5 == 0 and p1_choice == "Buzz") or (current_number % 15 == 0 and p1_choice == "FizzBuzz"):    
         if (current_number % 3 == 0 and p1_choice == -6) or (current_number % 5 == 0 and p1_choice == -10) or (current_number % 15 == 0 and p1_choice == -30):    
             try:
                 if p1_choice == -6:
@@ -31,13 +29,13 @@ while True:
                     p1_choice = "Buzz"
                 elif p1_choice == -30:
                     p1_choice = "FizzBuzz"        
-                print(f"P1 saied: {p1_choice}")
+                print(f"P1 said: {p1_choice}")
             except:
-                print(f"P1 saied: {p1_choice}")
+                print(f"P1 said: {p1_choice}")
                     
                 
         elif p1_choice % 3 != 0 and p1_choice % 5 != 0 and int(p1_choice) % 15 != 0 and p1_choice == current_number:
-            print(f"P1 saied: {p1_choice}")
+            print(f"P1 said: {p1_choice}")
         else:
             if p1_choice == -6:
                 p1_choice = "Fizz"
@@ -45,5 +43,5 @@ while True:
                 p1_choice = "Buzz"
             elif p1_choice == -30:    
                 p1_choice = "FizzBuzz"
-            print(f"Game is Over!\nP1 saied: {p1_choice} but current level must say Fizz, Buzz or FizzBuzz")    
+            print(f"Game is Over!\nP1 said: {p1_choice} but current level must say Fizz, Buzz or FizzBuzz")    
  
